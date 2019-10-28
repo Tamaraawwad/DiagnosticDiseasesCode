35e4aba3d12e359f74a0b6da6e61f8a8cb9d43ea
Tamaraawwad <30650778+Tamaraawwad@users.noreply.github.com>
2019-10-28 13:10:21 +0200

    ff
    ff

--- DiagnosticDiseasesCode.R
+++ DiagnosticDiseasesCode.R
@@ -10,26 +10,55 @@ DATA_RAW = "C:/Users/enasa/OneDrive/Documents/Diagnostic diseases/data_raw",
 SHARED =  "C:/Users/enasa/OneDrive/Documents/Diagnostic diseases/results"
 )
 
-
 library(readxl)
 library(openxlsx)
-X2019 <- read_excel("C:/Users/enasa/OneDrive/Documents/Diagnostic diseases/Dr Shahinaz/Diag Data/2019.xlsx")
-View(X2019)
-names(X2019)
-dim(X2019)
-str(X2019)
+
+#creating vectors for data raw
+#DIAGNOSTIC DATA
+
+diag_2019 <- read_excel(file.path(org::PROJ$DATA_RAW, "Diag Data" ,"2019.xlsx"))
+diag_2018 <- read_excel(file.path(org::PROJ$DATA_RAW, "Diag Data" ,"2018.xlsx"))
+diag_2017 <- read_excel(file.path(org::PROJ$DATA_RAW, "Diag Data" ,"2017.xlsx"))
+diag_2016 <- read_excel(file.path(org::PROJ$DATA_RAW, "Diag Data" ,"2016.xlsx"))
 
 
-unique(X2019$`Diagnose Name`)
+# Drugs Data
+drug_2019 <- read_excel(file.path(org::PROJ$DATA_RAW, "Drugs Data" ,"2019.xlsx"))
+drug_2018 <- read_excel(file.path(org::PROJ$DATA_RAW, "Drugs Data" ,"2018.xlsx"))
+drug_2017 <- read_excel(file.path(org::PROJ$DATA_RAW, "Drugs Data" ,"2017.xlsx"))
+drug_2016 <- read_excel(file.path(org::PROJ$DATA_RAW, "Drugs Data" ,"2016.xlsx"))
+
+
+#Lab Data
+lab_2019 <- read_excel(file.path(org::PROJ$DATA_RAW, "Lab Data" ,"2019.xlsx"))
+lab_2018 <- read_excel(file.path(org::PROJ$DATA_RAW, "Lab Data" ,"2018.xlsx"))
+lab_2017 <- read_excel(file.path(org::PROJ$DATA_RAW, "Lab Data" ,"2017.xlsx"))
+lab_2016 <- read_excel(file.path(org::PROJ$DATA_RAW, "Lab Data" ,"2016.xlsx"))
+
+#Observation Data
+obs_2019 <- read_excel(file.path(org::PROJ$DATA_RAW, "observation Data" ,"2019.xlsx"))
+obs_2018 <- read_excel(file.path(org::PROJ$DATA_RAW, "observation Data" ,"2018.xlsx"))
+obs_2017 <- read_excel(file.path(org::PROJ$DATA_RAW, "observation Data" ,"2017.xlsx"))
+obs_2016 <- read_excel(file.path(org::PROJ$DATA_RAW, "observation Data" ,"2016.xlsx"))
+
+
+
+View(diag_2019)
+names(diag_2019)
+dim(diag_2019)
+str(diag_2019)
+
+
+unique(diag_2019$`Diagnose Name`)
 
 
 
 
-tam <- xtabs(~X2019$`Medical Order Id`+ X2019$'Diagnose Name',addNA = T)
+tam <- xtabs(~diag_2019$`Medical Order Id`+ diag_2019$'Diagnose Name',addNA = T)
 
 chisq.test(tam,)
 
-tam <- xtabs(~ X2019$'Diagnose Name')
+tam <- xtabs(~ diag_2019$'Diagnose Name')
 
 
 
@@ -49,7 +78,7 @@ barplot(tam, main="Diagnosis Distribution",
 
 
 
-counts <- table(X2019$'Diagnose Name')
+counts <- table(diag_2019$'Diagnose Name')
 
 barplot(counts, main="Car Distribution",
         xlab="Number of Gears")
@@ -59,7 +88,7 @@ barplot(counts, main="Car Distribution",
 library(gmodels)
 
 
-tam2 <- CrossTable (X2019$`Diagnose Name`,X2019$Organization,addNA=T)
+tam2 <- CrossTable (diag_2019$`Diagnose Name`,diag_2019$Organization,addNA=T)
 
 openxlsx :: write.xlsx(tam2, 
                        file.path("C:/Users/enasa/OneDrive/Documents/Diagnostic diseases/results",
@@ -68,17 +97,17 @@ openxlsx :: write.xlsx(tam2,
 
 
 #library(data.table)
-#setDT(X2019)
-#View(X2019)
-#X2019[,DMDIAG:=F]
-#xtabs(~X2019$DMDIAG)
+#setDT(diag_2019)
+#View(diag_2019)
+#diag_2019[,DMDIAG:=F]
+#xtabs(~diag_2019$DMDIAG)
 
-#X2019['Diagnose Name'%in% c("Addisonian crisis","Tumor lysis syndrome", "Vitamin deficiency, unspecified")                                                                                  
+#diag_2019['Diagnose Name'%in% c("Addisonian crisis","Tumor lysis syndrome", "Vitamin deficiency, unspecified")                                                                                  
 
 #                             ,DMDIAG:=T]
 
 
-# #table <- X2019[,
+# #table <- diag_2019[,
 #               
 #               .(
 #                 N=.N,
